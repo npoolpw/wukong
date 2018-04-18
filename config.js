ab2e6d8d1fb5afb2b7748426139b939e55dfe43a
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-04-18 12:06:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-var api = "http://apihost.com:port";
+var api = "http://ca.npool.pw:2310";
 
-var poolHost = "apihost.com";
+var poolHost = "ca.npool.pw";
 
 var blockExplorer = "http://blockexplorer.pw/coin";
 
-var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
+var cryptonatorWidget = ["BTC-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
@@ -15,5 +15,5 @@ var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchai
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"]
+        ["npool.pw/wukong", "http://ca.npool.pw:2310"]
 ];
