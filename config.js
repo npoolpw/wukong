a75ed24b2712c2f83db63d99ebcde207ee6cf501
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-04-18 12:03:35 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+var api = "http://apihost.com:port";
+
+var poolHost = "apihost.com";
+
+var blockExplorer = "http://blockexplorer.pw/coin";
+
+var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["coin.apihost.com", "http://coin.apihost.com:port"]
+];
